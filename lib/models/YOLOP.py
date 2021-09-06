6d49f8c702d09f479666eeaae3b615bbeea8e623
NAVINFO\liubao <liubao@navinfo.com>
2021-09-06 14:22:31 +0800

    修改网络结构： Focus——Conv（block#0） BottleneckCSP——Bottleneck

--- lib/models/YOLOP.py
+++ lib/models/YOLOP.py
@@ -452,50 +452,50 @@ MCnet_share1 = [
 # The lane line and the driving area segment branches without share information with each other and without link
 YOLOP = [
 [24, 33, 42],   #Det_out_idx, Da_Segout_idx, LL_Segout_idx
-[ -1, Focus, [3, 32, 3]],   #0
+[ -1, Conv, [3, 32, 3, 2]],    #0
 [ -1, Conv, [32, 64, 3, 2]],    #1
-[ -1, BottleneckCSP, [64, 64, 1]],  #2
+[ -1, Bottleneck, [64, 64, 1]],  #2
 [ -1, Conv, [64, 128, 3, 2]],   #3
-[ -1, BottleneckCSP, [128, 128, 3]],    #4
+[ -1, Bottleneck, [128, 128, 3]],    #4
 [ -1, Conv, [128, 256, 3, 2]],  #5
-[ -1, BottleneckCSP, [256, 256, 3]],    #6
+[ -1, Bottleneck, [256, 256, 3]],    #6
 [ -1, Conv, [256, 512, 3, 2]],  #7
 [ -1, SPP, [512, 512, [5, 9, 13]]],     #8
-[ -1, BottleneckCSP, [512, 512, 1, False]],     #9
+[ -1, Bottleneck, [512, 512, 1, False]],     #9
 [ -1, Conv,[512, 256, 1, 1]],   #10
 [ -1, Upsample, [None, 2, 'nearest']],  #11
 [ [-1, 6], Concat, [1]],    #12
-[ -1, BottleneckCSP, [512, 256, 1, False]], #13
+[ -1, Bottleneck, [512, 256, 1, False]], #13
 [ -1, Conv, [256, 128, 1, 1]],  #14
 [ -1, Upsample, [None, 2, 'nearest']],  #15
 [ [-1,4], Concat, [1]],     #16         #Encoder
 
-[ -1, BottleneckCSP, [256, 128, 1, False]],     #17
+[ -1, Bottleneck, [256, 128, 1, False]],     #17
 [ -1, Conv, [128, 128, 3, 2]],      #18
 [ [-1, 14], Concat, [1]],       #19
-[ -1, BottleneckCSP, [256, 256, 1, False]],     #20
+[ -1, Bottleneck, [256, 256, 1, False]],     #20
 [ -1, Conv, [256, 256, 3, 2]],      #21
 [ [-1, 10], Concat, [1]],   #22
-[ -1, BottleneckCSP, [512, 512, 1, False]],     #23
+[ -1, Bottleneck, [512, 512, 1, False]],     #23
 [ [17, 20, 23], Detect,  [1, [[3,9,5,11,4,20], [7,18,6,39,12,31], [19,50,38,81,68,157]], [128, 256, 512]]], #Detection head 24
 
 [ 16, Conv, [256, 128, 3, 1]],   #25
 [ -1, Upsample, [None, 2, 'nearest']],  #26
-[ -1, BottleneckCSP, [128, 64, 1, False]],  #27
+[ -1, Bottleneck, [128, 64, 1, False]],  #27
 [ -1, Conv, [64, 32, 3, 1]],    #28
 [ -1, Upsample, [None, 2, 'nearest']],  #29
 [ -1, Conv, [32, 16, 3, 1]],    #30
-[ -1, BottleneckCSP, [16, 8, 1, False]],    #31
+[ -1, Bottleneck, [16, 8, 1, False]],    #31
 [ -1, Upsample, [None, 2, 'nearest']],  #32
 [ -1, Conv, [8, 2, 3, 1]], #33 Driving area segmentation head
 
 [ 16, Conv, [256, 128, 3, 1]],   #34
 [ -1, Upsample, [None, 2, 'nearest']],  #35
-[ -1, BottleneckCSP, [128, 64, 1, False]],  #36
+[ -1, Bottleneck, [128, 64, 1, False]],  #36
 [ -1, Conv, [64, 32, 3, 1]],    #37
 [ -1, Upsample, [None, 2, 'nearest']],  #38
 [ -1, Conv, [32, 16, 3, 1]],    #39
-[ -1, BottleneckCSP, [16, 8, 1, False]],    #40
+[ -1, Bottleneck, [16, 8, 1, False]],    #40
 [ -1, Upsample, [None, 2, 'nearest']],  #41
 [ -1, Conv, [8, 2, 3, 1]] #42 Lane line segmentation head
 ]
